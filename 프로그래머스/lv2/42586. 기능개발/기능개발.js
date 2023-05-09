142938cd08a9b1a39b466758bced67d2855f11f0
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-05-09 18:19:02 +0900

    [level 2] Title: 기능개발, Time: 3.16 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -0,0 +1,26 @@
+function solution(progresses, speeds) {
+    let answer = [];
+    let working = progresses;
+    let workload = speeds;
+    if (progresses.length !== speeds.length)
+        return answer;
+    
+    while(working.length){
+        if (working[0] >= 100){ // 배포
+            // finshed : 배포 완료된 기능
+            let finshed = [];
+            while(working[0] >= 100)
+            {
+                finshed.push(working.shift());
+                workload.shift();
+            }
+            console.log(working)
+            answer.push(finshed.length); // 배포 시점 완료된 작업 추가
+        }
+        // ~기능 개발~
+        working = working.map((data,idx)=>data+workload[idx]);
+    }
+    
+    
+    return answer;
+}

6430cef3a7ee1988ea4b6a8fa16ae8ce1846afe4
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-05-22 16:34:19 +0900

    Update 모의고사.js
    주석 추가

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,5 +1,6 @@
 function solution(answers) {
     let answer = [];
+    // 정답을 몇개 맞았는지 체크하는 함수
     const checker = (method)=>{
         let correct = 0;
         for (let i=0; i<answers.length; i++)
@@ -11,10 +12,13 @@ function solution(answers) {
         }
         return correct;
     }
+    // 수포자 별 정답 체크
     const [supoja1 ,supoja2 , supoja3] = [ checker([1,2,3,4,5]),checker([2,1,2,3,2,4,2,5]),checker([3, 3, 1, 1, 2, 2, 4, 4, 5, 5]) ]
     
+    // 가장 높은 점수 체크 
     const topScore = [supoja1 ,supoja2 , supoja3].reduce((total,cur,idx)=>total < cur ? cur : total, 0);
     
+    // 가장 높은 점수를 가진 수포자를 `answer` 배열에 입력
    [supoja1 ,supoja2 , supoja3].forEach((cur, idx)=>{
         if (topScore === cur)
             answer.push(idx+1);

1a34a57fb7ce50b529912d3a1ddf1ad81bd69b0c
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-07-05 11:18:57 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,17 +1,4 @@
 function solution(my_string) {
     var answer = '';
-    
-    for (e of my_string)
-    {
-        const position = answer.indexOf(e);
-        if (-1 === position)
-        {
-            answer += e;
-        }
-    }
-
-
-    
-    
     return answer;
 }

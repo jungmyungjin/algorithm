8bc84855b0e30aef02eafe6f54853a7c5f758e9b
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-07-06 11:17:18 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,15 +1,4 @@
 function solution(s) {
     var answer = 0;
-    let nums = s.split(' ').reverse();
-    
-    for(let i=0; i<nums.length; i++)
-    {
-        if (nums[i] === 'Z')
-        {
-            i +=1;
-            continue;
-        }
-        answer += parseInt(nums[i]);
-    }
     return answer;
 }

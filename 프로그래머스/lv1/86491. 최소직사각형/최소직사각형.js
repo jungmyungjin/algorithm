7aa32c935c8d6020054b36006d902699f88a14b8
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-07-12 11:29:15 +0900

    [level 1] Title: 최소직사각형, Time: 3.25 ms, Memory: 39.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,13 +1,4 @@
 function solution(sizes) {
     var answer = 0;
-    
-    // 가로가 더 긴 명함지갑
-    let [maxWidth, maxHeight] = [0,0];
-    
-    sizes.forEach(e=>{
-        let [bigger, smaller] = (e[0] > e[1])? [e[0],e[1]] : [e[1], e[0]];
-        maxWidth = maxWidth < bigger ? bigger : maxWidth;
-        maxHeight = maxHeight < smaller ? smaller : maxHeight;
-    })    
-    return maxWidth*maxHeight;
+    return answer;
 }

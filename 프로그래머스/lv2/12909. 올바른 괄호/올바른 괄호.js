af2a71d5257a1bfec9f39c2c122fd0282337d897
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-07-25 11:41:45 +0900

    [level 2] Title: 올바른 괄호, Time: 4.77 ms, Memory: 37.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,30 @@
+
+
+
+
+function solution(s){
+    var answer = true;
+    let stacks = [];
+
+    for (let i=0; i<s.length; i++)
+    {
+        if ( s[i] === ')' && stacks.length === 0)
+        {
+            return false;
+        }
+        
+        if (s[i] ==='(')
+        {
+            stacks.push('1');
+        }
+        else if (s[i] === ')' && stacks.length > 0)
+        {
+            stacks.pop();
+        }
+    }
+    if (stacks.length !== 0)
+        return false;
+    
+
+    return true;
+}

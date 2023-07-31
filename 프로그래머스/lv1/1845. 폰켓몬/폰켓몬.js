5b0e965f8e6865a767bae8a50bd950286a674e1c
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-07-31 11:21:57 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,7 @@
 function solution(nums) {
-    var answer = 0;
-    let max = Math.floor(nums.length / 2);
-    let set = new Set(nums);
-    let kind = [...set];
-    console.log(kind)
+    const setNums = new Set(nums);
+    const typeCount = [... setNums].length;
     
-    return kind.length > max ? max: kind.length;
+    
+    return nums.length/2 >= typeCount ? typeCount: nums.length / 2;
 }

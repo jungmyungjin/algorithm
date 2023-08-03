c30e3eb17681aebc883df90e6217df3afb0fc5c1
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-03 12:07:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,27 +1,8 @@
 function solution(n, words) {
-    var answer = [0,0];
+    var answer = [];
 
-    
-    for (let turn = 1; turn<=words.length; turn++)
-    {
-        const user = turn%n === 0 ? n :turn%n;
-        if (turn === 1)
-            continue;
-        const idx = turn-1;
-        const prev_last_string = words[idx-1].substr(-1,1);
-        const curr_start_string = words[idx].substr(0,1);
-        if (prev_last_string !== curr_start_string)
-        {
-            return [user,Math.ceil(turn/n)];
-        }
-
-        const prev_words = words.slice(0,idx);
-        const current_word = words[idx];
-        if (prev_words.includes(current_word))
-        {
-            return [user,Math.ceil(turn/n)];
-        }
-    }
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+    console.log('Hello Javascript')
 
     return answer;
 }

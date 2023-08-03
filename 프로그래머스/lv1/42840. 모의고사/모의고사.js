3beb151a37818deb87a79a3fcfc3e3e14e5c1f4c
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-03 11:35:32 +0900

    [level 1] Title: 모의고사, Time: 3.53 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,29 +1,27 @@
 function solution(answers) {
-    let answer = [];
-    // 정답을 몇개 맞았는지 체크하는 함수
-    const checker = (method)=>{
-        let correct = 0;
-        for (let i=0; i<answers.length; i++)
-        {
-            if (answers[i] === method[i%method.length])
-            {
-                correct++;
-            }
-        }
-        return correct;
-    }
-    // 수포자 별 정답 체크
-    const [supoja1 ,supoja2 , supoja3] = [ checker([1,2,3,4,5]),checker([2,1,2,3,2,4,2,5]),checker([3, 3, 1, 1, 2, 2, 4, 4, 5, 5]) ]
+    let supozaMethod = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]];
+    let supozaScore = [0,0,0];
     
-    // 가장 높은 점수 체크 
-    const topScore = [supoja1 ,supoja2 , supoja3].reduce((total,cur,idx)=>total < cur ? cur : total, 0);
     
-    // 가장 높은 점수를 가진 수포자를 `answer` 배열에 입력
-   [supoja1 ,supoja2 , supoja3].forEach((cur, idx)=>{
-        if (topScore === cur)
-            answer.push(idx+1);
-    })
+    for(let i=0; i<answers.length; i++)
+    {
+        const grading = answers[i];
+        if(grading === supozaMethod[0][i%supozaMethod[0].length])
+            supozaScore[0]++
+        if(grading === supozaMethod[1][i%supozaMethod[1].length])
+            supozaScore[1]++
+        if(grading === supozaMethod[2][i%supozaMethod[2].length])
+            supozaScore[2]++
+    }    
+    let bestScore = supozaScore.reduce((total, current)=>total<current? current : total,0) ;
     
+    let answer =[];
+    if (bestScore === supozaScore[0])
+        answer.push(1);
+    if (bestScore === supozaScore[1])
+        answer.push(2);
+    if (bestScore === supozaScore[2])
+        answer.push(3);
     
     return answer;
 }

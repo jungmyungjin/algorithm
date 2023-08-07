a0c77e792c3a4d5e1e09986fe91312d3bff1be08
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-07 12:19:56 +0900

    [level 2] Title: 피보나치 수, Time: 3.38 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/README.md
+++ 프로그래머스/lv2/12945. 피보나치 수/README.md
@@ -1,6 +1,6 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -1,28 +1,11 @@
 function solution(n) {
     var answer = 0;
-    
-    let num1=1
-    let num2=1;
-    
-   
-    for (let i =2; i<n; i++)
+    let storage = [0,1];
+    for(let idx = 2; idx <= n; idx++)
     {
-        let [n1,n2] = [num1, num2];
-        num1 = n2;
-        num2 = (n1+n2) % 1234567;
+        storage.push((storage[0] + storage[1]) % 1234567);
+        storage.shift();
     }
-    return num2 ;
+    answer = storage[1];
+    return answer;
 }
-
-
-// function solution(n) {
-//     var answer = 0;
-//     let storage = [0,1];
-//     for(let idx = 2; idx <= n; idx++)
-//     {
-//         storage.push((storage[0] + storage[1]) % 1234567);
-//         storage.shift();
-//     }
-//     answer = storage[1];
-//     return answer;
-// }

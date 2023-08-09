f9fcc6dd360660b8c262fa78bbae964e09b48a27
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-09 11:20:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.74 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,14 +1,8 @@
 function solution(A,B){
     var answer = 0;
-    
-    A.sort((a,b)=>a-b);
-    B.sort((a,b)=>b-a);
 
-    for(let i=0; i<A.length; i++)
-    {
-        answer += A[i] * B[i];
-    }
-
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
 
     return answer;
 }

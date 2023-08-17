6633227022c05fe067116d473b35f7bb0a0a54e1
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-17 11:59:35 +0900

    [level 2] Title: H-Index, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,17 +1,18 @@
 function solution(citations) {
-    var answer = 0;
-    citations.sort((a,b)=>{return a-b});
-    console.log(citations);
-    for(let i=0; i<citations.length; i++)
+    citations.sort((a,b)=>b-a);
+    for(let i=1; i<=citations.length; i++)
+    {
+        // i 이상 인용
+        let up = 0;
+        for(let j=0; j<citations.length; j++)
         {
-            const num = citations.length - i;
-            if (num == citations[num -1] && 
-                ((num-1 > 0  && num != citations[num -2]) ||
-               (num-1 == 1 && num != citations[num -2]))
-               )
-            {
-                answer = num;
-            }
+            if (i < citations[j])
+                up++
         }
-    return answer;
+        
+        if (i === up)
+            return i;
+    }
+    
+    return 0;
 }

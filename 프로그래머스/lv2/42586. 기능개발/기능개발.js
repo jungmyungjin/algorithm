10a76aa50eb3ebcd2b0dfbf9c817e2b5f7ecc834
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-28 19:17:14 +0900

    [level 2] Title: 기능개발, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,56 +1,22 @@
 function solution(progresses, speeds) {
+    let answer = [];
+    let [working,workload] = [progresses, speeds];
     
-    const time_taken = Array.from({length :progresses.length}, ()=>0);
-    progresses.forEach((progress, idx)=>{
-        while(progress < 100)
-        {
-            time_taken[idx]++;
-            progress += speeds[idx];
+    while(working.length){
+        if (working[0] >= 100){ // 배포
+            // finshed : 배포 완료된 기능
+            let finshed = [];
+            while(working[0] >= 100)
+            {
+                finshed.push(working.shift());
+                workload.shift();
+            }
+            answer.push(finshed.length); // 배포 시점 완료된 작업 추가
         }
-    })
-    
-    console.log(time_taken)
-    const answer = [];
-    let [max_day, count_day] = [0,0];
-    for(let day of time_taken)
-    {
-        // 제일 큰 날짜보다 크면, 배포
-        if (max_day < day)
-        {
-            if (max_day !== 0) answer.push(count_day);
-            max_day = day;
-            count_day = 1;
-        }
-        else{
-            count_day++;
-        }
+        // ~기능 개발~
+        working = working.map((data,idx)=>data+workload[idx]);
     }
-    answer.push(count_day)
-
+    
     
     return answer;
 }
-
-// 이전에 푼 내용
-// function solution(progresses, speeds) {
-//     let answer = [];
-//     let [working,workload] = [progresses, speeds];
-    
-//     while(working.length){
-//         if (working[0] >= 100){ // 배포
-//             // finshed : 배포 완료된 기능
-//             let finshed = [];
-//             while(working[0] >= 100)
-//             {
-//                 finshed.push(working.shift());
-//                 workload.shift();
-//             }
-//             answer.push(finshed.length); // 배포 시점 완료된 작업 추가
-//         }
-//         // ~기능 개발~
-//         working = working.map((data,idx)=>data+workload[idx]);
-//     }
-    
-    
-//     return answer;
-// }

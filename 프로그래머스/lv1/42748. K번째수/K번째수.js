488c53c2d6c85784d599897450c7c9ac0b816c4a
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-28 11:26:34 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,10 +1,22 @@
 function solution(array, commands) {
     var answer = [];
-
-    commands.forEach((element)=>{
-        const tmp = array.slice(element[0]-1,element[1]);
-        tmp.sort((a,b)=>{return a-b});
-        answer.push(tmp[element[2]-1]);
-    });
+    
+    commands.forEach((nums)=>{
+        const sliced = array.slice(nums[0]-1, nums[1]);
+        sliced.sort((a,b)=>a-b);
+        answer.push(sliced.at(nums[2]-1));
+    })
     return answer;
 }
+
+// 이전 코드
+// function solution(array, commands) {
+//     var answer = [];
+
+//     commands.forEach((element)=>{
+//         const tmp = array.slice(element[0]-1,element[1]);
+//         tmp.sort((a,b)=>{return a-b});
+//         answer.push(tmp[element[2]-1]);
+//     });
+//     return answer;
+// }

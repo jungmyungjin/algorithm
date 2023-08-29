4dce67875939a65bc15fa68d9e324d086fd1a04b
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-08-29 11:25:20 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,44 @@
+function dayOfWeek(startDay, days) {
+  // days = parseInt(days);
+  const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  var deduplicatedDays = startDay,
+    resultDay = "";
+
+  deduplicatedDays = (deduplicatedDays + days) % 7;
+  resultDay = week[deduplicatedDays];
+
+  return resultDay;
+}
+
+function solution(a, b) {
+  var answer = "001";
+  const month = parseInt(a),
+    day = parseInt(b);
+  var currentDayPointer = 5;
+  var days = 0;
+
+  const lastDay = {
+    31: [1, 3, 5, 7, 8, 10, 12],
+    30: [4, 6, 9, 11],
+    29: [2]
+  };
+
+  // 월
+  for (var index = 1; index < month; index++) {
+    if (index < month) {
+      Object.keys(lastDay).forEach(element => {
+        element = parseInt(element);
+        if (lastDay[element].includes(index)) {
+          days = days + parseInt(element);
+        }
+      });
+    }
+  }
+
+  //일
+  days = days + (day - 1);
+
+  answer = dayOfWeek(5, days);
+
+  return answer;
+}

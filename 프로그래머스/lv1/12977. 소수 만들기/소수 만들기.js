c3dd1191ed8ddc722e57ce2069b06ca03cc77cbb
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2023-09-12 11:33:24 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -1,31 +1,61 @@
-function check_prime(number)
-{
-    for (let idx = 2; idx <= Math.floor(Math.sqrt(number)) ; idx++)
-    {
-        if (number % idx == 0)
-        {
-            return (false);
-        }
-    }
-    return (true);
-}
+
 
 function solution(nums) {
     var answer = 0;
-    check_prime();
-
-    const length = nums.length;
-    for(let x =0; x < length; x++)
+    
+    for(let i=0; i<nums.length; i++)
     {
-        for(let y = x+1; y < length; y++)
+        for(let j=i+1; j<nums.length; j++)
         {
-            for (let z = y + 1; z < length; z++)
+            for(let k=j+1; k<nums.length; k++)
             {
-                if (check_prime(nums[x]+nums[y]+nums[z]))
-                    answer += 1;
+                const sum = nums[i]+nums[j]+nums[k];
+                let isPrime = true;
+                for(let s=2; s<=Math.round(Math.sqrt(sum)); s++)
+                {
+                    if (sum%s === 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                    answer++
             }
         }
     }
-
+        
     return answer;
 }
+
+// function check_prime(number)
+// {
+//     for (let idx = 2; idx <= Math.floor(Math.sqrt(number)) ; idx++)
+//     {
+//         if (number % idx == 0)
+//         {
+//             return (false);
+//         }
+//     }
+//     return (true);
+// }
+
+// function solution(nums) {
+//     var answer = 0;
+//     check_prime();
+
+//     const length = nums.length;
+//     for(let x =0; x < length; x++)
+//     {
+//         for(let y = x+1; y < length; y++)
+//         {
+//             for (let z = y + 1; z < length; z++)
+//             {
+//                 if (check_prime(nums[x]+nums[y]+nums[z]))
+//                     answer += 1;
+//             }
+//         }
+//     }
+
+//     return answer;
+// }
